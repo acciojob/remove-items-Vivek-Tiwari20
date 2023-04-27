12625e42f7e76f6387c07b89470336a1e05a09ac
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-27 20:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+  const button = document.getElementById("button");
+
+        button.addEventListener("click",showinput);
+        function showinput() {
+        var x=document.getElementById("colorSelect");
+        x.remove(x.selectedIndex);
+        }
